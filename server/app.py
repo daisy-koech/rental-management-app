cd9a62f7115a89de5378b19030f579c1f171fdf7
Daisy Koech <koechdaisy86@gmail.com>
2026-08-27 19:41:55 +0300

    configure Flask secret key

--- server/app.py
+++ server/app.py
@@ -3,10 +3,12 @@ from flask_cors import CORS
 from config import Config
 from extensions import db, migrate, bcrypt
 from routes import register_routes
+import os
 
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = os.environ.get("SECRET_KEY", "dev-only-fallback-change-me")
 
     app.config.from_object(Config)
 
