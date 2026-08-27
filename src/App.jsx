f4d39fbe763375929d4c2866a5c4c8ef9054091f
Daisy Koech <koechdaisy86@gmail.com>
2026-08-28 00:46:07 +0300

    add leaflet map rendering and notice date + time spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "leaflet/dist/leaflet.css";
+import "./leafletIconFix"
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 
